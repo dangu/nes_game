6295ed741f1af0b1fee21c7f9df9dd2c7dade684
Daniel Gullberg <daniel_gullberg@hotmail.com>
2017-06-29 21:34:04 +0200

    Interesting. With this code, nesasm segfaults (nesasm 1ddfe1e17753)

--- src/main.asm
+++ src/main.asm
@@ -43,6 +43,51 @@ Start:
 	stx $2001	; Disable rendering (https://wiki.nesdev.com/w/index.php/PPU_registers#PPUMASK)
 	stx $4010	; Disable DMC interrupts (https://wiki.nesdev.com/w/index.php/APU_DMC)
 
+	; Code from https://wiki.nesdev.com/w/index.php/Init_code:
+    ; If the user presses Reset during vblank, the PPU may reset
+    ; with the vblank flag still true.  This has about a 1 in 13
+    ; chance of happening on NTSC or 2 in 9 on PAL.  Clear the
+    ; flag now so the @vblankwait1 loop sees an actual vblank.
+	; Something about the BIT instruction (http://users.telenet.be/kim1-6502/6502/proman.html#4221):
+	; BIT: Test bits in memory with accumulator
+	; The instruction affects the N flag (the 7th bit) but doesn't store
+	; the result in the accumulator
+    bit $2002
+
+    ; First of two waits for vertical blank to make sure that the
+    ; PPU has stabilized
+@vblankwait1:  
+    bit $2002
+    bpl @vblankwait1
+    
+    ; We now have about 30,000 cycles to burn before the PPU stabilizes.
+    ; One thing we can do with this time is put RAM in a known state.
+    ; Here we fill it with $00, which matches what (say) a C compiler
+    ; expects for BSS.  Conveniently, X is still 0.
+    txa
+@clrmem:
+    sta $000,x
+    sta $100,x
+    sta $300,x
+    sta $400,x
+    sta $500,x
+    sta $600,x
+    sta $700,x  ; Remove this if you're storing reset-persistent data
+
+    ; We skipped $200,x on purpose.  Usually, RAM page 2 is used for the
+    ; display list to be copied to OAM.  OAM needs to be initialized to
+    ; $EF-$FF, not 0, or you'll get a bunch of garbage sprites at (0, 0).
+
+    inx
+    bne @clrmem
+
+    ; Other things you can do between vblank waits are set up audio
+    ; or set up other mapper registers.
+   
+@vblankwait2:
+    bit $2002
+    bpl @vblankwait2
+    
 
 	jsr init
 	cli			; Enable interrupts
